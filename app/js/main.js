b56256ad5d71fc2b27c555c2ca9a36b7fcf4d4d3
TetianaDro <tdrobovska@gmail.com>
2021-08-24 17:23:54 +0300

    added brands, insta, made adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -3,26 +3,50 @@ mySwiper = new Swiper ('.new-event__slider', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
+       pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
       slidesPerView: 1,
       spaceBetween: 30,
-      // pagination: {
-      //   el: '.swiper-pagination',
-      //   clickable: true,
-      // },
-      // breakpoints: {
-      //   475: {
-      //     slidesPerView: 1.35,
-      //   },
-      //   318: {
-      //     slidesPerView: 1,
-      //   },
-      // }
   });
+
+  
 mySwiper = new Swiper ('.new-arrived__slider', {
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      slidesPerView: 3,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },      
+      breakpoints: {
+        760: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        620: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        318: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+      }
+  });
+
+
+mySwiper = new Swiper ('.brands__slider', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },      
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }, 
+      slidesPerView: 1,
       spaceBetween: 30,
   });
